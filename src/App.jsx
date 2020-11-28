dbdde33f5b6e827ea4ebe9ffbd33cb139372a0b6
kotodu <37821512+busroutemap@users.noreply.github.com>
2020-11-28 20:52:30 +0900

    📝コメントの更新

--- src/App.jsx
+++ src/App.jsx
@@ -4,47 +4,79 @@ import PaintArea from "./components/PaintArea";
 import SizePicker from "./components/SizePicker";
 import { APP } from "./const";
 
+/**
+ * Appの中心的コンポーネント
+ */
 class App extends React.Component {
     constructor(props) {
         super(props);
+
+        // stateの初期化
         this.state = {
             /**
              * @property {number} 線幅
              * @default 4
+             * @description 既定値を参照する
              */
             lineWidth: APP.LINE_WIDTH,
 
             /**
              * @property {string} 枠色
              * @default "#0040FF"
+             * @description 既定値を参照する
              */
             lineColor: APP.LINE_COLOR
         }
     }
 
     render() {
+        const lineWidth = this.state.lineWidth;
+        const lineColor = this.state.lineColor;
+
+        // ヘッダー部
+        const title = (
+            <h1>タイトル</h1>
+        );
+
+        // サイズピッカー
+        const sizePicker = (
+            <SizePicker
+                lineWidth={lineWidth}
+                setWidth={(newWidth) => {
+                    this.setState({
+                        lineWidth:newWidth
+                    });
+                }}
+            />
+        );
+
+        // カラーピッカー
+        const colorPicker = (
+            <ColorPicker
+                color={lineColor}
+                setColor={(newColor) => {
+                    this.setState({
+                        lineColor:newColor
+                    });
+                }}
+            />
+        );
+
+        // 描画エリア
+        const paintArea = (
+            <PaintArea
+                lineColor={lineColor}
+                lineWidth={lineWidth}
+            />
+        );
+
         // 描画内容
         return (
             <div>
-                <h1>タイトル</h1>
-                <SizePicker
-                    lineWidth={this.state.lineWidth}
-                    setWidth={(lineWidth) => this.setState({
-                        lineWidth
-                    })}
-                />
-                <ColorPicker
-                    color={this.state.lineColor}
-                    setColor={(lineColor) => {
-                        this.setState({
-                            lineColor
-                        })
-                    }}
-                />
-                <PaintArea
-                    lineColor={this.state.lineColor}
-                    lineWidth={this.state.lineWidth}
-                />
+                {title}
+                {sizePicker}
+                {colorPicker}
+                {paintArea}
             </div>
         );
     }
--- src/components/ColorPicker.jsx
+++ src/components/ColorPicker.jsx
@@ -1,24 +1,57 @@
+import React from "react";
+
+
+// type Props = {
+//     color: String,
+//     setColor:(newColor:string)=>void
+// }
+
+//---------------------------------------------
+
+/**
+ * 
+ * @param {any} props 
+ * @returns {JSX.Element}
+ */
 const ColorPicker = (props) => {
+    // 引数からの受け取り
     const color = props.color;
+
+    // label
+    const pickerLabel = (
+        <label
+            htmlFor="color-picker"
+            className="form-label"
+        >
+            Color Picker
+        </label>
+    );
+
+    // picker本体
+    const pickerInput = (
+        <input
+            type="color"
+            className="form-control form-control-color"
+            id="color-picker"
+            value={color}
+            title="Choose your color"
+            onChange={(e) => {
+                // 新たな色を上位コンポーネントへ渡す
+                const newColor = e.target.value;
+                props.setColor(newColor);
+            }}
+        />
+    );
+
+
+    // 描画内容
     return (
         <div>
-            <label
-                htmlFor="exampleColorInput"
-                className="form-label"
-            >
-                Color picker
-                </label>
-            <input
-                type="color"
-                className="form-control form-control-color"
-                id="exampleColorInput"
-                value={color}
-                title="Choose your color"
-                onChange={(e) => {
-                    props.setColor(e.target.value);
-                }}
-            />
+            {pickerLabel}
+            {pickerInput}
         </div>
     );
 }
+
+// デフォルトエクスポート
 export default ColorPicker;
