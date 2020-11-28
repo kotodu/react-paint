b0d4b992636240813385eeac6b343c3d171ba348
kotodu <37821512+busroutemap@users.noreply.github.com>
2020-11-28 22:44:30 +0900

    ♻️SizePickerのRange化

--- src/components/SizePicker.jsx
+++ src/components/SizePicker.jsx
@@ -1,4 +1,7 @@
-
+/**
+ * 
+ * @param {*} props 
+ */
 const SizePicker = (props) => {
     /**
      * @var {number} lineWidth 線幅
@@ -7,21 +10,28 @@ const SizePicker = (props) => {
      */
     const lineWidth = props.lineWidth;
 
+    const widthInput = (
+        <input
+            value={lineWidth}
+            type="range"
+            min={1}
+            max={40}
+            step={0.5}
+            className="form-lange"
+            onChange={(e) => {
+                // 新たな線幅を上位コンポーネントへ
+                const newWidth = Number(e.target.value);
+                props.setWidth(newWidth);
+            }}
+        />
+    );
     // 描画内容
     return (
         <div>
             <h2>
                 {lineWidth}px
             </h2>
-            <p>
-                <input
-                    value={lineWidth}
-                    type={"number"}
-                    onChange={(e) => {
-                        props.setWidth(Number(e.target.value));
-                    }}
-                />
-            </p>
+            {widthInput}
         </div>
     );
 }
