b96240fe46ded5230bef610a2f946eb34244c8b6
kotodu <37821512+busroutemap@users.noreply.github.com>
2020-11-30 19:23:34 +0900

    ✏️lange→range

--- src/components/SizePicker.jsx
+++ src/components/SizePicker.jsx
@@ -17,7 +17,7 @@ const SizePicker = (props) => {
             min={1}
             max={40}
             step={0.5}
-            className="form-lange col-6"
+            className="form-range col-6"
             onChange={(e) => {
                 // 新たな線幅を上位コンポーネントへ
                 const newWidth = Number(e.target.value);
